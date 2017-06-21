53566ed97475e8030f60141ac86b55e696ac5a66
Joe Huss <detain@interserver.net>
2017-06-21 19:20:11 -0400

    minor include fix

--- src/Plugin.php
+++ src/Plugin.php
@@ -40,7 +40,7 @@ class Plugin {
 	public static function doEnable(\Service_Order $serviceOrder) {
 		$serviceInfo = $serviceOrder->getServiceInfo();
 		$settings = get_module_settings($serviceOrder->get_module());
-		require_once 'include/licenses/license.functions.inc.php';
+		require_once __DIR__.'/../../../../include/licenses/license.functions.inc.php';
 		function_requirements('activate_fantastico');
 		activate_fantastico($serviceInfo[$settings['PREFIX'].'_ip'], 2);
 		$GLOBALS['tf']->history->add($settings['TABLE'], 'add_fantastico', $serviceInfo[$settings['PREFIX'].'_id'], $serviceInfo[$settings['PREFIX'].'_ip'], $serviceInfo[$settings['PREFIX'].'_custid']);
