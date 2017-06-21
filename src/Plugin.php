5edf842e77a835800dc7d9011aecbdacf0deefbf
Joe Huss <detain@interserver.net>
2017-06-21 16:10:48 -0400

    camelCase fix

--- src/Plugin.php
+++ src/Plugin.php
@@ -31,13 +31,13 @@ class Plugin {
 			->set_text('Fantastico')
 			->set_cost(VPS_FANTASTICO_COST)
 			->set_require_ip(TRUE)
-			->set_enable([__CLASS__, 'Enable'])
-			->set_disable([__CLASS__, 'Disable'])
+			->set_enable([__CLASS__, 'doEnable'])
+			->set_disable([__CLASS__, 'doDisable'])
 			->register();
 		$service->add_addon($addon);
 	}
 
-	public static function Enable(\Service_Order $serviceOrder) {
+	public static function doEnable(\Service_Order $serviceOrder) {
 		$serviceInfo = $serviceOrder->getServiceInfo();
 		$settings = get_module_settings($serviceOrder->get_module());
 		require_once 'include/licenses/license.functions.inc.php';
@@ -46,7 +46,7 @@ class Plugin {
 		$GLOBALS['tf']->history->add($settings['TABLE'], 'add_fantastico', $serviceInfo[$settings['PREFIX'].'_id'], $serviceInfo[$settings['PREFIX'].'_ip'], $serviceInfo[$settings['PREFIX'].'_custid']);
 	}
 
-	public static function Disable(\Service_Order $serviceOrder) {
+	public static function doDisable(\Service_Order $serviceOrder) {
 	}
 
 	public static function getSettings(GenericEvent $event) {
