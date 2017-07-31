89129d380a6e0eb703fb6e53f1f6822b155fa8d7
Joe Huss <detain@interserver.net>
2017-07-30 23:35:11 -0400

    refactoring some calls

--- src/Plugin.php
+++ src/Plugin.php
@@ -44,8 +44,8 @@ class Plugin {
 			->set_text('Fantastico')
 			->set_cost(VPS_FANTASTICO_COST)
 			->set_require_ip(TRUE)
-			->set_enable([__CLASS__, 'doEnable'])
-			->set_disable([__CLASS__, 'doDisable'])
+			->setEnable([__CLASS__, 'doEnable'])
+			->setDisable([__CLASS__, 'doDisable'])
 			->register();
 		$service->addAddon($addon);
 	}
