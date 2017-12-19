986561f7c9073934f5557c65b9c3e0a686d5a7b0
Joe Huss <detain@interserver.net>
2017-12-19 18:52:38 -0500

    adding function and requirements for function to handle adding the addon

---
+++ src/vps_add_fantastico.php
@@ -0,0 +1,19 @@
+<?php
+	/**
+	* VPS Functionality
+	* @author Joe Huss <detain@interserver.net>
+	* @copyright 2017
+	* @package MyAdmin
+	* @category VPS
+	*/
+
+	/**
+	 * Adds Fantastico to a VPS
+	 * @return void
+	 */
+	function vps_add_fantastico() {
+		function_requirements('class.AddServiceAddon');
+		$addon = new AddServiceAddon();
+		$addon->load(__FUNCTION__, 'Fantastico', 'vps', VPS_FANTASTICO_COST);
+		$addon->process();
+	}
